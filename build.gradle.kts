ea80c9c2e7e92f870ed255aa5858e1cb3de72f64
Alexander Staeding <alexanderstaeding@gmail.com>
2022-02-05 14:26:13 +0100

    New grader structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,45 +1,106 @@
+import org.sourcegrade.submitter.submit
+
 plugins {
     java
+    application
     id("org.sourcegrade.style") version "1.2.0"
+    id("org.sourcegrade.submitter") version "0.4.0"
+}
+
+version = "0.1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
+}
+
+submit {
+    assignmentId = "h07"
+    studentId = "ab12cdef"
+    firstName = "sol_first"
+    lastName = "sol_last"
+}
+
+val grader: SourceSet by sourceSets.creating {
+    val test = sourceSets.test.get()
+    compileClasspath += test.compileClasspath
+    runtimeClasspath += output + compileClasspath + test.runtimeClasspath
+}
+
+dependencies {
+    implementation("org.jetbrains:annotations:23.0.0")
+    "graderCompileOnly"("org.sourcegrade:jagr-launcher:0.4.0-SNAPSHOT")
+    "graderImplementation"("fr.inria.gforge.spoon:spoon-core:10.0.0")
+    "graderImplementation"("org.sourcegrade:docwatcher-api:0.1")
+    "graderImplementation"("org.mockito:mockito-core:4.3.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+}
+
+application {
+    mainClass.set("h07.Main")
 }
 
 tasks {
-    create<Jar>("graderJar") {
+    val runDir = File("build/run")
+    named<JavaExec>("run") {
+        doFirst {
+            runDir.mkdirs()
+        }
+        workingDir = runDir
+    }
+    test {
+        doFirst {
+            runDir.mkdirs()
+        }
+        workingDir = runDir
+        useJUnitPlatform()
+    }
+    val graderTest by creating(Test::class) {
+        group = "verification"
+        doFirst {
+            runDir.mkdirs()
+        }
+        workingDir = runDir
+        testClassesDirs = grader.output.classesDirs
+        classpath = grader.runtimeClasspath
+        useJUnitPlatform()
+    }
+    named("check") {
+        dependsOn(graderTest)
+    }
+    val graderJar by creating(Jar::class) {
         group = "build"
         afterEvaluate {
             archiveFileName.set("FOP-2022-H07-${project.version}.jar")
-            from(project(":grader").sourceSets.main.get().allSource)
-            from(project(":solution").sourceSets.main.get().allSource)
+            from(sourceSets.main.get().allSource)
+            from(sourceSets.test.get().allSource)
+            from(grader.allSource)
         }
     }
-}
-
-allprojects {
-    apply(plugin = "java")
-    apply(plugin = "org.sourcegrade.style")
-    version = "1.0.0-SNAPSHOT"
-    repositories {
-        mavenCentral()
-        maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
-    }
-    java {
-        withSourcesJar()
-    }
-    dependencies {
-        implementation("org.sourcegrade:jagr-grader-api:0.3")
-        implementation("org.junit.jupiter:junit-jupiter:5.8.2")
-    }
-    tasks {
-        withType<JavaCompile> {
-            options.encoding = "UTF-8"
-            sourceCompatibility = "11"
-            targetCompatibility = "11"
+    val graderLibs by creating(Jar::class) {
+        group = "build"
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        val runtimeDeps = grader.runtimeClasspath.mapNotNull {
+            if (it.path.toLowerCase().contains("h07")) {
+                null
+            } else if (it.isDirectory) {
+                it
+            } else {
+                zipTree(it)
+            }
         }
-        jar {
-            archiveFileName.set("${rootProject.name}-${project.name}.jar")
-        }
-        named<Jar>("sourcesJar") {
-            archiveFileName.set("${rootProject.name}-${project.name}-sources.jar")
-        }
+        from(runtimeDeps)
+        archiveFileName.set("h07-libs.jar")
+    }
+    create("graderAll") {
+        dependsOn(graderJar, graderLibs)
+    }
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+    jar {
+        enabled = false
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,1 @@
 rootProject.name = "FOP-2022-H07-Root"
-include("FOP-2022-H07-Student")
-include("grader")
-include("solution")
